2b471b46a6991ef88a5201b62336f302c6c38e64
zj9785-prog <zj9785@srmist.edu.in>
2026-02-24 14:19:59 +0530

    uc3

---
+++ src/UC5.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class UC5{
+
+    public static boolean isPalindrome(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        // Push characters into stack
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        // Pop and compare
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String input = "level";
+
+        if (isPalindrome(input)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
